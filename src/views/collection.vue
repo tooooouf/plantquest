f851902f079730c4d9c79267aaa96b34ff50b3ba
tooouf <ne191024@senshu-u.jp>
2021-09-25 22:15:36 +0900

    gh pages docs

---
+++ src/views/collection.vue
@@ -0,0 +1,54 @@
+<template>
+  <Header headerText="コレクション"/>
+  <section class="collection">
+    <div class="box">
+      <h2>一覧</h2>
+      <div class="questList">
+        <div class="questCard" v-for="(quest, key) in quests" :key="key">
+          <router-link class="questLink" to="/collection_detail"/>
+          <div class="imgWrap">
+            <img class="questImg" v-bind:src="quest.plantImage" alt="">
+          </div>
+          <div class="questDesctiption">
+            <h3 class="plantName">{{quest.plantName}}</h3>
+            <p>{{quest.plantSummery}}</p>
+            <p>レア度：{{quest.questRarity}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Header from '@/components/header.vue'
+
+export default {
+  components:{
+    Header
+  },
+
+  data(){
+    return{
+      quests: [
+        {
+          plantName: 'パンジー',
+          plantImage: require('@/assets/img/DSCF0676.jpg'),
+          plantSummery: 'スミレ科スミレ属の園芸植物',
+          questSummery: 'パンジーを発見し、撮影しよう。',
+          questReward: '××マート　50円引クーポン',
+          questRarity: '★★☆☆☆',
+        },
+        {
+          plantName: 'タンポポ',
+          plantImage: require('@/assets/img/DSCF0827.jpg'),
+          plantSummery: 'キク科タンポポ属の植物',
+          questSummery: 'タンポポを発見し、撮影しよう。',
+          questReward: null,
+          questRarity: '★☆☆☆☆',
+        },
+      ]
+    }
+  },
+}
+</script>
---
+++ src/views/coupons.vue
@@ -0,0 +1,66 @@
+<template>
+  <Header headerText="クーポン"/>
+  <section class="coupons">
+    <div class="box">
+      <h2>一覧</h2>
+      <div class="questList" v-for="(coupon, key) in coupons" :key="key">
+        <div class="questCard">
+          <div class="imgWrap">
+            <img class="couponImg" src="@/assets/img/couponticket.svg" alt="">
+          </div>
+          <div class="questDesctiption">
+            <h3 class="couponName">{{coupon.couponShop}}</h3>
+            <p>{{coupon.couponSummery}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+import Header from '@/components/header.vue'
+
+export default {
+  components:{
+    Header
+  },
+
+  data(){
+    return{
+      coupons: [
+        {
+          couponShop: '○○レストラン',
+          couponSummery: '100円引クーポン',
+        },
+        {
+          couponShop: '△△ショップ',
+          couponSummery: '20円引クーポン',
+        },
+        {
+          couponShop: '□□コンビニ',
+          couponSummery: '70円引クーポン',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.questCard{
+  height: 100px;
+}
+
+.imgWrap{
+  display: flex;
+
+}
+
+.couponImg{
+  width: 100%;
+}
+
+</style>
