2706394388295e9b27a7cd9216782f48d6b8680f
tooouf <ne191024@senshu-u.jp>
2021-09-28 21:44:29 +0900

    result ios fix

--- src/views/result.vue
+++ src/views/result.vue
@@ -3,7 +3,7 @@
   <section class="result">
     <div class="box">
       <h2>パンジー</h2>
-      <img src="@/assets/img/DSCF0675.jpg" alt="">
+      <img src="@/assets/img/DSCF0675.jpg" alt="" class="resultImg">
       <p>スミレ科スミレ属の園芸植物</p>
       <p>冬から春にかけて開花が始まる。寒さにとても強く、雪に埋もれても、寒さで凍っても損傷することなく暖かくなれば元通り活動を再開することができる。交配によってさまざまな色が存在する。日本では夏が暑すぎて枯れてしまうが、寒い地域では夏を越し、もう1年生きる多年草である。</p>
     </div>
@@ -40,6 +40,10 @@ export default {
 
 .getcoupon{
   height: auto;
+}
+
+.resultImg{
+  width: 100%;
 }
 
 </style>
